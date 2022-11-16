4fe48b4885419c949664f104ff25a9164623a566
Igor de Andrade da Cunha <igoradacunha@hotmail.com>
2022-11-16 10:06:00 -0300

    Treinamento

---
+++ Treinamentos/Udemy_React_Webpack/src/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import React from 'react' 
+import { render } from 'react-dom'
+import Title from './app'
+
+render(
+	React.createElement(Title),
+	document.querySelector('[data-js="app"]')
+)
